20102acc5975243807889501ebe003453db07d1f
Админ <shikhragimovr@yandex.ru>
2024-11-22 23:19:00 +0500

    сделано дз 4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int age = 18;
-        if (age >= 18) {
+        if ( age >= 18) {
             System.out.println("он совершеннолетний");
         } else {
             System.out.println("он не совершеннолетний нужно немного подождать");
