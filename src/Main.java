052f1283877e0843642504f991a3a6959a2b2a5b
Админ <shikhragimovr@yandex.ru>
2024-11-22 23:41:41 +0500

    сделано дз 4

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,77 @@ public class Main {
             System.out.println("он совершеннолетний");
         } else {
             System.out.println("он не совершеннолетний нужно немного подождать");
+
+
         }
+        int temperature = 5;
+        if (temperature > 5) {
+            System.out.println("нужно надеть шапку");
+        } else {
+            System.out.println("не нужно надевать шапку");
+
+        }
+        int speed = 60;
+        if (speed > 60) {
+            System.out.println("скорость превышена нужно заплатить штраф");
+        } else {
+            System.out.println("можно ехать спокойно");
+
+        }
+        int people = 17;
+        if (people > 2 && people <=6) {
+            System.out.println("нужно ходить в детский сад");
+        } else {
+        if (people > 7 && people <=17) {
+            System.out.println("нужно ходить в школу");
+        } else {
+        if (people >= 18 && people <= 24) {
+            System.out.println("ему пора в университет");
+        } else {
+        if (people > 24) {
+            System.out.println("пора ходить на работу");
+
+        }
+        int atraction = 8;
+        if (atraction < 5) {
+            System.out.println("он не может кататься на аттракционе");
+        } else {
+        if (atraction > 5 && atraction <= 14) {
+            System.out.println("он может кататься только в сопроождении взрослого");
+        } else {
+        if (atraction > 14) {
+            System.out.println("он может кататься без мопровождение взрослого");
+
+        }
+        int vagon = 102;
+        if (vagon > 1 && vagon <= 60) {
+            System.out.println("есть сидячие места");
+        } else {
+        if (vagon > 62 && vagon <= 102) {
+            System.out.println("есть стоячие места");
+        } else {
+        if (vagon > 102) {
+            System.out.println("вагон забит");
+
+        }
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        if (a < b) {
+            System.out.println("1 меньше 2");
+        } else {
+        if (b < c) {
+            System.out.println("2 меньше 3");
+        }
+        }
+        }
+        }
+        }
+        }
+        }
+
+            }
+        }
+
     }
 }
